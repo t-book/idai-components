a865ce060b1eba61e52f66bc7634bbf2b4a13e3e
Daniel M. de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2015-07-16 14:12:30 +0200

    Add navbar to components.

---
+++ src/js/directives_navbar.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/* Directives */
+angular.module('idai.directives')
+
+
+/**
+ * @author: Daniel M. de Oliveira
+ */
+
+	.directive('arNavbar', function() {
+		return {
+			restrict: 'E',
+			scope: {},
+			templateUrl: 'partials/directives/ar-navbar.html',
+			controller: [ '$scope', '$http', 'localizedContent', '$location',
+				function($scope,$http, localizedContent, $location) {
+
+					$scope.getNavbarLinks = function(contentDir){
+						$http.get(contentDir+'/content.json').success(function(data){
+							var navbarLinks = localizedContent.getNodeById(data,'navbar');
+							if (navbarLinks==undefined) {console.log('error: no navbarLinks found');}
+							localizedContent.reduceTitles(navbarLinks)
+							$scope.dynamicLinkList=navbarLinks.children;
+						});
+					}
+
+					$scope.search = function(fq) {
+						if ($scope.q) {
+							var url = '/search?q=' + $scope.q;
+							if (fq) url += "&fq=" + fq;
+							$scope.q = null;
+							$location.url(url);
+						}
+					}
+
+				}],
+			link: function(scope,element,attrs){
+				scope.getNavbarLinks(attrs.contentDir);
+			}
+		}});
