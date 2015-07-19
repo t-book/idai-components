1bad3ae24dd3bb17f94772e7be88864a3dd41e09
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2015-07-19 11:13:33 +0200

    Make transl8 filter stateful so variable hack is no longer needed.

--- src/js/filters_transl8.js
+++ src/js/filters_transl8.js
@@ -3,12 +3,15 @@
 /* Services */
 angular.module('idai.filters')
 
-	/**
-	 * Author: Daniel M. de Oliveira
-	 */
-	.filter('transl8', ['transl8',function(transl8){
-		return function(key) {
-			var trans = transl8.getTranslation(key);
-			return trans;
-		}
-	}]);
+/**
+ * Author: Daniel M. de Oliveira
+ */
+.filter('transl8', ['transl8',function(transl8){
+	
+	var filterFunction = function(key) {
+		var trans = transl8.getTranslation(key);
+		return trans;
+	}
+	filterFunction.$stateful=true;
+	return filterFunction;
+}]);
