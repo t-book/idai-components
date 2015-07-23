f09b29d3699102b44ba793ebbd2ec56ab81bf4e5
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-07-23 08:56:40 +0200

    automatically set current year in footer

--- src/js/directives_footer.js
+++ src/js/directives_footer.js
@@ -14,14 +14,15 @@ return {
 	templateUrl: 'partials/directives/idai-footer.html',
 	controller: [ '$scope', '$http', 'localizedContent', 
 		function($scope,$http, localizedContent) {
-		$scope.getFooterLinks = function(contentDir){
-			$http.get('info/content.json').success(function(data){
-				var footerLinks = localizedContent.getNodeById(data,'footer');
-				if (footerLinks==undefined) {console.log('error: no footerLinks found');}
-				localizedContent.reduceTitles(footerLinks)	
-				$scope.dynamicLinkList=footerLinks.children;
-			});				
-		}
+			$scope.date = new Date();
+			$scope.getFooterLinks = function(contentDir){
+				$http.get('info/content.json').success(function(data){
+					var footerLinks = localizedContent.getNodeById(data,'footer');
+					if (footerLinks==undefined) {console.log('error: no footerLinks found');}
+					localizedContent.reduceTitles(footerLinks)	
+					$scope.dynamicLinkList=footerLinks.children;
+				});				
+			}
 		}],
 	link: function(scope,element,attrs){
 		scope.getFooterLinks(attrs.contentDir);
