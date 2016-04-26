d18d5c2e85a8738c54316aac3232953cd95f7291
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-04-26 14:42:15 +0200

    removed console log

--- src/js/directives/picker.js
+++ src/js/directives/picker.js
@@ -95,7 +95,6 @@ angular.module('idai.components')
 
 		$scope.keydown = function($event) {
 			// arrow down preselects next item
-			console.log($event.keyCode);
 			if ($event.keyCode == 40 && $scope.preselect < $scope.result.length - 1) {
 				$scope.preselect++;
 			// arrow up select precious item
