2cfdada59e734626f3e56e7329a6fa2d61de843f
Jan G. Wieners <jan@jan-wieners.de>
2016-09-12 17:12:27 +0200

    Build new idai-components version.

--- dist/idai-components-no-tpls.js
+++ dist/idai-components-no-tpls.js
@@ -32,6 +32,334 @@ angular.module('idai.components')
 
 angular.module('idai.components')
 
+
+/**
+ * @author: Daniel M. de Oliveira
+ */
+
+    .directive('idaiFooter', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                mailto: '@',
+                institutions: '=',
+                version: '@'
+            },
+            transclude: true,
+            templateUrl: 'partials/directives/idai-footer.html',
+            controller: ['$scope', '$http', '$sce', 'localizedContent', '$transclude',
+                function ($scope, $http, $sce, localizedContent, $transclude) {
+
+                    $transclude(function(clone){
+                        $scope.hasTranscludedContent = (clone.length > 0);
+                    });
+
+                    $scope.date = new Date();
+                    $scope.getFooterLinks = function (contentDir) {
+
+                        $http.get('info/content.json').success(function (data) {
+                            var footerLinks = localizedContent.getNodeById(data, 'footer');
+                            if (footerLinks == undefined) {
+                                console.log('error: no footerLinks found');
+                            }
+                            localizedContent.reduceTitles(footerLinks);
+                            $scope.dynamicLinkList = footerLinks.children;
+                        });
+                    }
+                }],
+            link: function (scope, element, attrs) {
+
+                scope.getFooterLinks(attrs.contentDir);
+            }
+        }
+    });
+'use strict';
+
+angular.module('idai.components')
+
+/** 
+ * @author: Sebastian Cuy
+ */
+
+ .directive('idaiForm', function() {
+	return {
+		restrict: 'E',
+        transclude: true,
+		scope: {
+			submit: '&', doc: '='
+		},
+		templateUrl: 'partials/directives/idai-form.html',
+		link: function(scope, elem, attrs) {
+
+			scope.reset = function() {
+				scope.doc = {};
+			};
+
+		}
+	}
+});
+
+'use strict';
+
+angular.module('idai.components')
+
+    /**
+     * @author: Jan G. Wieners
+     */
+    .directive('idaiHeader', function () {
+        return {
+            restrict: 'E',
+            //replace: 'true',
+            scope: {
+                image: '@',
+                description: '@',
+                link: '@'
+            },
+            templateUrl: 'partials/directives/idai-header.html'
+        }
+    });
+'use strict';
+
+/* Directives */
+angular.module('idai.components')
+
+
+/**
+ * @author: Daniel M. de Oliveira
+ */
+
+.directive('includeReplace', function () {
+    return {
+       	require: 'ngInclude',
+       	restrict: 'A', /* optional */
+       	link: function (scope, el, attrs) {
+           	el.replaceWith(el.children());
+       	}  
+	}});
+'use strict';
+
+angular.module('idai.components')
+
+
+/**
+ * @author: Daniel M. de Oliveira
+ */
+
+.directive('idaiMessage', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/directives/idai-message.html',
+        controller: [ '$scope', 'message',
+            function($scope,message) {
+
+                $scope.messages = message.getMessages();
+
+                $scope.removeMessage = function(transl8Key){
+                    message.removeMessage(transl8Key)
+                };
+
+            }]
+    }});
+'use strict';
+
+/* Directives */
+angular.module('idai.components')
+
+
+/**
+ * @author: Daniel de Oliveira
+ */
+
+	.directive('idaiNavbar', function() {
+		return {
+			restrict: 'E',
+			scope: {
+				userObject: '=',
+				loginFunction: '&',
+				logoutFunction: '&',
+				hideSearchForm: '=',
+				hideRegisterButton: '=', // set "true" to hide it
+				hideContactButton: '=', // set "true" to hide it
+				projectId: '@'
+			},
+			templateUrl: 'partials/directives/idai-navbar.html',
+			controller: [ '$scope', '$http', 'localizedContent', '$location',
+				function($scope, $http, localizedContent, $location) {
+
+					$scope.getNavbarLinks = function(contentDir){
+						$http.get('info/content.json').success(function(data){
+							var navbarLinks = localizedContent.getNodeById(data,'navbar');
+							if (navbarLinks==undefined) {console.log('error: no navbarLinks found');}
+							localizedContent.reduceTitles(navbarLinks)
+							$scope.dynamicLinkList=navbarLinks.children;
+						});
+					};
+
+					$scope.search = function(fq) {
+						if ($scope.q) {
+							var url = '/search?q=' + $scope.q;
+							if (fq) url += "&fq=" + fq;
+							$scope.q = null;
+							$location.url(url);
+						}
+					};
+
+					$scope.toggleNavbar = function() {
+
+							$scope.isCollapsed = true;
+							$scope.$on('$routeChangeSuccess', function () {
+								$scope.isCollapsed = true;
+							});
+					};
+					
+				}],
+			link: function(scope,element,attrs){
+				scope.getNavbarLinks(attrs.contentDir);
+			}
+		}});
+'use strict';
+
+angular.module('idai.components')
+
+/** 
+ * @author: Sebastian Cuy
+ */
+
+ .directive('idaiPicker', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			searchUri: '@',	resultField: '@', titleField: '@',
+			totalField: '@', queryParam: '@', limitParam: '@',
+			offsetParam: '@', addParams: '=', selectedItem: '='
+		},
+		templateUrl: 'partials/directives/idai-picker.html',
+		controller: [ '$scope', '$parse', '$uibModal',
+			function($scope, $parse, $modal) {
+
+				$scope.openModal = function() {
+					var modal = $modal.open({
+						templateUrl: "picker_modal.html",
+						controller: "PickerModalController",
+						bindToController: true,
+						size: 'lg',
+						scope: $scope
+					});
+					modal.result.then(function(item) {
+						$scope.selectedItem = item;
+					});
+				};
+
+				$scope.$watch("titleField", function(titleField) {
+					if (!titleField) titleField = "title";
+					$scope.getTitleField = $parse(titleField);
+				});
+
+			}
+		]
+	}
+})
+
+.controller('PickerModalController', [ '$scope', '$http', '$q', '$parse', '$uibModalInstance',
+	function($scope, $http, $q, $parse, $modalInstance) {
+		
+		var canceler;
+
+		$scope.result;
+		$scope.total = 0;
+		$scope.offset = 0;
+		$scope.limit = 10;
+		$scope.loading = false;
+		$scope.preselect = 0;
+
+		var search = function() {
+			if (canceler) canceler.resolve();
+			if ($scope.query) {
+				$scope.loading = true;
+				canceler = $q.defer();
+				if (!$scope.queryParam) $scope.queryParam = "q";
+				if (!$scope.limitParam) $scope.limitParam = "limit";
+				if (!$scope.offsetParam) $scope.offsetParam = "offset";
+				var requestUri = $scope.searchUri + "?" + $scope.queryParam + "=" + $scope.query;
+				requestUri += "&" + $scope.limitParam + "=" + $scope.limit;
+				requestUri += "&" + $scope.offsetParam + "=" + $scope.offset;
+				if ($scope.addParams) {
+					angular.forEach($scope.addParams, function(value, key) {
+						requestUri += "&" + key + "=" + value;
+					});
+				}
+				$http.get(requestUri, { timeout: canceler.promise }).then(function(response) {
+					if (!$scope.resultField) $scope.resultField = "result";
+					var getResultField = $parse($scope.resultField);
+					if ($scope.offset == 0) {
+						$scope.result = getResultField(response.data);
+					} else {
+						$scope.result = $scope.result.concat(getResultField(response.data));
+					}
+					if (!$scope.totalField) $scope.totalField = "total";
+					var getTotalField = $parse($scope.totalField);
+					$scope.total = getTotalField(response.data);
+					$scope.loading = false;
+				});
+			} else {
+				$scope.result = [];
+				$scope.total = 0;
+			}
+		};
+
+		$scope.more = function() {
+			$scope.offset += $scope.limit;
+			search();
+		};
+
+		$scope.keydown = function($event) {
+			// arrow down preselects next item
+			if ($event.keyCode == 40 && $scope.preselect < $scope.result.length - 1) {
+				$scope.preselect++;
+			// arrow up select precious item
+			} else if ($event.keyCode == 38 && $scope.preselect > 0) {
+				$scope.preselect--;
+			}
+		};
+
+		$scope.keypress = function($event) {
+			// enter selects preselected item (if query has not changed)
+			if ($event.keyCode == 13) {
+				if ($scope.total > 0 && $scope.query == $scope.lastQuery) {
+					$event.stopPropagation();
+					$scope.selectItem($scope.result[$scope.preselect]);
+				} else {
+					$scope.newQuery();
+				}
+			}
+		};
+
+		$scope.newQuery = function() {
+			$scope.lastQuery = $scope.query;
+			$scope.offset = 0;
+			search();
+		};
+
+		$scope.open = function(uri) {
+			window.open(uri, '_blank');
+		};
+
+		$scope.selectItem = function(item) {
+			$modalInstance.close(item);
+		};
+
+		$scope.$watch("titleField", function(titleField) {
+			if (!titleField) titleField = "title";
+			$scope.getTitleField = $parse(titleField);
+		});
+
+	}
+]);
+
+'use strict';
+
+angular.module('idai.components')
+
     /**
      * @author: Jan G. Wieners
      */
@@ -476,315 +804,3 @@ angular.module('idai.components')
 
 	}
 }]);
-'use strict';
-
-angular.module('idai.components')
-
-
-/** 
- * @author: Daniel M. de Oliveira
- */
-
-.directive('idaiFooter', function() {
-return {
-	restrict: 'E',
-	scope: { mailto: '@', institutions: '=', version: '@' },
-	templateUrl: 'partials/directives/idai-footer.html',
-	controller: [ '$scope', '$http', 'localizedContent', 
-		function($scope,$http, localizedContent) {
-			$scope.date = new Date();
-			$scope.getFooterLinks = function(contentDir){
-				$http.get('info/content.json').success(function(data){
-					var footerLinks = localizedContent.getNodeById(data,'footer');
-					if (footerLinks==undefined) {console.log('error: no footerLinks found');}
-					localizedContent.reduceTitles(footerLinks)	
-					$scope.dynamicLinkList=footerLinks.children;
-				});				
-			}
-		}],
-	link: function(scope,element,attrs){
-		scope.getFooterLinks(attrs.contentDir);
-	}
-}});
-'use strict';
-
-angular.module('idai.components')
-
-/** 
- * @author: Sebastian Cuy
- */
-
- .directive('idaiForm', function() {
-	return {
-		restrict: 'E',
-        transclude: true,
-		scope: {
-			submit: '&', doc: '='
-		},
-		templateUrl: 'partials/directives/idai-form.html',
-		link: function(scope, elem, attrs) {
-
-			scope.reset = function() {
-				scope.doc = {};
-			};
-
-		}
-	}
-});
-
-'use strict';
-
-angular.module('idai.components')
-
-    /**
-     * @author: Jan G. Wieners
-     */
-    .directive('idaiHeader', function () {
-        return {
-            restrict: 'E',
-            //replace: 'true',
-            scope: {
-                image: '@',
-                description: '@',
-                link: '@'
-            },
-            templateUrl: 'partials/directives/idai-header.html'
-        }
-    });
-'use strict';
-
-/* Directives */
-angular.module('idai.components')
-
-
-/**
- * @author: Daniel M. de Oliveira
- */
-
-.directive('includeReplace', function () {
-    return {
-       	require: 'ngInclude',
-       	restrict: 'A', /* optional */
-       	link: function (scope, el, attrs) {
-           	el.replaceWith(el.children());
-       	}  
-	}});
-'use strict';
-
-angular.module('idai.components')
-
-
-/**
- * @author: Daniel M. de Oliveira
- */
-
-.directive('idaiMessage', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'partials/directives/idai-message.html',
-        controller: [ '$scope', 'message',
-            function($scope,message) {
-
-                $scope.messages = message.getMessages();
-
-                $scope.removeMessage = function(transl8Key){
-                    message.removeMessage(transl8Key)
-                };
-
-            }]
-    }});
-'use strict';
-
-/* Directives */
-angular.module('idai.components')
-
-
-/**
- * @author: Daniel de Oliveira
- */
-
-	.directive('idaiNavbar', function() {
-		return {
-			restrict: 'E',
-			scope: {
-				userObject: '=',
-				loginFunction: '&',
-				logoutFunction: '&',
-				hideSearchForm: '=',
-				hideRegisterButton: '=', // set "true" to hide it
-				hideContactButton: '=', // set "true" to hide it
-				projectId: '@'
-			},
-			templateUrl: 'partials/directives/idai-navbar.html',
-			controller: [ '$scope', '$http', 'localizedContent', '$location',
-				function($scope, $http, localizedContent, $location) {
-
-					$scope.getNavbarLinks = function(contentDir){
-						$http.get('info/content.json').success(function(data){
-							var navbarLinks = localizedContent.getNodeById(data,'navbar');
-							if (navbarLinks==undefined) {console.log('error: no navbarLinks found');}
-							localizedContent.reduceTitles(navbarLinks)
-							$scope.dynamicLinkList=navbarLinks.children;
-						});
-					};
-
-					$scope.search = function(fq) {
-						if ($scope.q) {
-							var url = '/search?q=' + $scope.q;
-							if (fq) url += "&fq=" + fq;
-							$scope.q = null;
-							$location.url(url);
-						}
-					};
-
-					$scope.toggleNavbar = function() {
-
-							$scope.isCollapsed = true;
-							$scope.$on('$routeChangeSuccess', function () {
-								$scope.isCollapsed = true;
-							});
-					};
-					
-				}],
-			link: function(scope,element,attrs){
-				scope.getNavbarLinks(attrs.contentDir);
-			}
-		}});
-'use strict';
-
-angular.module('idai.components')
-
-/** 
- * @author: Sebastian Cuy
- */
-
- .directive('idaiPicker', function() {
-	return {
-		restrict: 'E',
-		scope: {
-			searchUri: '@',	resultField: '@', titleField: '@',
-			totalField: '@', queryParam: '@', limitParam: '@',
-			offsetParam: '@', addParams: '=', selectedItem: '='
-		},
-		templateUrl: 'partials/directives/idai-picker.html',
-		controller: [ '$scope', '$parse', '$uibModal',
-			function($scope, $parse, $modal) {
-
-				$scope.openModal = function() {
-					var modal = $modal.open({
-						templateUrl: "picker_modal.html",
-						controller: "PickerModalController",
-						bindToController: true,
-						size: 'lg',
-						scope: $scope
-					});
-					modal.result.then(function(item) {
-						$scope.selectedItem = item;
-					});
-				};
-
-				$scope.$watch("titleField", function(titleField) {
-					if (!titleField) titleField = "title";
-					$scope.getTitleField = $parse(titleField);
-				});
-
-			}
-		]
-	}
-})
-
-.controller('PickerModalController', [ '$scope', '$http', '$q', '$parse', '$uibModalInstance',
-	function($scope, $http, $q, $parse, $modalInstance) {
-		
-		var canceler;
-
-		$scope.result;
-		$scope.total = 0;
-		$scope.offset = 0;
-		$scope.limit = 10;
-		$scope.loading = false;
-		$scope.preselect = 0;
-
-		var search = function() {
-			if (canceler) canceler.resolve();
-			if ($scope.query) {
-				$scope.loading = true;
-				canceler = $q.defer();
-				if (!$scope.queryParam) $scope.queryParam = "q";
-				if (!$scope.limitParam) $scope.limitParam = "limit";
-				if (!$scope.offsetParam) $scope.offsetParam = "offset";
-				var requestUri = $scope.searchUri + "?" + $scope.queryParam + "=" + $scope.query;
-				requestUri += "&" + $scope.limitParam + "=" + $scope.limit;
-				requestUri += "&" + $scope.offsetParam + "=" + $scope.offset;
-				if ($scope.addParams) {
-					angular.forEach($scope.addParams, function(value, key) {
-						requestUri += "&" + key + "=" + value;
-					});
-				}
-				$http.get(requestUri, { timeout: canceler.promise }).then(function(response) {
-					if (!$scope.resultField) $scope.resultField = "result";
-					var getResultField = $parse($scope.resultField);
-					if ($scope.offset == 0) {
-						$scope.result = getResultField(response.data);
-					} else {
-						$scope.result = $scope.result.concat(getResultField(response.data));
-					}
-					if (!$scope.totalField) $scope.totalField = "total";
-					var getTotalField = $parse($scope.totalField);
-					$scope.total = getTotalField(response.data);
-					$scope.loading = false;
-				});
-			} else {
-				$scope.result = [];
-				$scope.total = 0;
-			}
-		};
-
-		$scope.more = function() {
-			$scope.offset += $scope.limit;
-			search();
-		};
-
-		$scope.keydown = function($event) {
-			// arrow down preselects next item
-			if ($event.keyCode == 40 && $scope.preselect < $scope.result.length - 1) {
-				$scope.preselect++;
-			// arrow up select precious item
-			} else if ($event.keyCode == 38 && $scope.preselect > 0) {
-				$scope.preselect--;
-			}
-		};
-
-		$scope.keypress = function($event) {
-			// enter selects preselected item (if query has not changed)
-			if ($event.keyCode == 13) {
-				if ($scope.total > 0 && $scope.query == $scope.lastQuery) {
-					$event.stopPropagation();
-					$scope.selectItem($scope.result[$scope.preselect]);
-				} else {
-					$scope.newQuery();
-				}
-			}
-		};
-
-		$scope.newQuery = function() {
-			$scope.lastQuery = $scope.query;
-			$scope.offset = 0;
-			search();
-		};
-
-		$scope.open = function(uri) {
-			window.open(uri, '_blank');
-		};
-
-		$scope.selectItem = function(item) {
-			$modalInstance.close(item);
-		};
-
-		$scope.$watch("titleField", function(titleField) {
-			if (!titleField) titleField = "title";
-			$scope.getTitleField = $parse(titleField);
-		});
-
-	}
-]);
