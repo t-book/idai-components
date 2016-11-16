767510a1e3b7256cbc000337f295e55fd0dfa0cb
Wolfgang Schmidle <wolfgang.schmidle@dainst.de>
2016-11-16 10:17:22 +0100

    separate transl8 file for components

--- src/js/services/transl8.js
+++ src/js/services/transl8.js
@@ -16,18 +16,27 @@ angular.module('idai.components')
 			var translationsLoaded = false;
 			var translations = {}; // Map: [transl8_key,translation].
 
-
 			if (language.browserPrimaryLanguage()==GERMAN_LANG) translationLang=GERMAN_LANG;
-			var transl8Url = componentsSettings.transl8Uri.replace('{LANG}',translationLang);
+			var transl8UrlComponents = "http://bogusman01.dai-cloud.uni-koeln.de/transl8/translation/jsonp?application=components1&lang={LANG}&callback=JSON_CALLBACK".replace('{LANG}',translationLang);
+			var transl8UrlApplication = componentsSettings.transl8Uri.replace('{LANG}',translationLang);
 
-			var promise = $http.jsonp(transl8Url).success(function(data) {
+			var promise = $http.jsonp(transl8UrlComponents).success(function(data) {
+				for(var i = 0; i < data.length; i++) {
+					translations[data[i].key] = data[i].value;
+				}
+			}).
+			error(function() {
+				alert("ERROR: Could not get components translations. Try to reload the page or send a mail to arachne@uni-koeln.de");
+			});
+
+			promise = $http.jsonp(transl8UrlApplication).success(function(data) {
 				for(var i = 0; i < data.length; i++) {
 					translations[data[i].key] = data[i].value;
 				}
 				translationsLoaded = true;
 			}).
 			error(function() {
-				alert("ERROR: Could not get translations. Try to reload the page or send a mail to arachne@uni-koeln.de");
+				alert("ERROR: Could not get application translations. Try to reload the page or send a mail to arachne@uni-koeln.de");
 			});
 
 			return {
