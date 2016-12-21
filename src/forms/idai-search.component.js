2872877afa6abfe7775d329776bf963606c54a0e
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-12-21 15:50:44 +0100

    Make that older searches are kept and suggested in search field.

--- src/forms/idai-search.component.js
+++ src/forms/idai-search.component.js
@@ -5,6 +5,7 @@ angular.module('idai.components')
 
 /**
  * @author Patrick Jominet
+ * @author Jan Wieners
  * @author Daniel de Oliveira
  */
 .component('idaiSearch', {
@@ -16,28 +17,69 @@ angular.module('idai.components')
     controller: [ '$scope', '$location', 'componentsSettings', '$http',
         function($scope,$location,componentsSettings,$http) {
 
+            var localStorageKey='previousSearchQueries';
+
             $scope.buttonClass = 'btn-primary';
             if (this.buttonClass) {
                 $scope.buttonClass = this.buttonClass;
             }
 
-            $scope.search = function ($item, $model, $label, $event) {
-                if ($scope.q) {
-                    var url = '/search?q=' + $scope.q;
-                    $scope.q = null;
-                    $location.url(url);
+            $scope.search = function ($item) {
+
+                var searchTerm;
+                if ($item) {
+                    searchTerm = $item.model;
+                } else {
+                    searchTerm = $scope.q;
                 }
+                memorizeSearch(searchTerm,3);
+
+                var url = '/search?q=' + searchTerm;
+                $scope.q = null;
+                $location.url(url);
+
             };
 
             $scope.getSuggestions = function (value) {
                 if (!componentsSettings.searchUri) return;
 
-                var promise = $http.get(componentsSettings.searchUri + value)
+                return $http.get(componentsSettings.searchUri + value)
                     .then(function (response) {
-                        return response.data.suggestions;
+
+                        var suggestions=
+                            response.data.suggestions.map(function(e){return {model:e}});
+
+                        var queries = JSON.parse(localStorage.getItem(localStorageKey));
+                        queries.reverse();
+                        queries.forEach(function(term){
+                          suggestions.push({model:term,extra:true})
+                        });
+
+                        return suggestions;
                     });
-                if(promise != 'undefined') return promise;
             };
+
+            function memorizeSearch(searchTerm,searchesToKeep) {
+
+                var queries = JSON.parse(localStorage.getItem(localStorageKey));
+
+                if (queries === null || !queries) {
+                    queries = [searchTerm];
+                } else {
+
+                    queries.push(searchTerm);
+
+                    // Make unique
+                    queries = queries.filter(function(item, pos) {
+                        return queries.indexOf(item) == pos;
+                    });
+
+                    if (queries.length > searchesToKeep +1) {
+                        queries.shift();
+                    }
+                }
+                localStorage.setItem(localStorageKey, JSON.stringify(queries));
+            }
         }
     ]
 });
